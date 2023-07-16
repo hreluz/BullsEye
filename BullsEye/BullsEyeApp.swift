4be25811f42cc1b17d57746ef798404da53e813b
Hector <hmreluz@gmail.com>
2023-07-15 21:30:05 -0500

    Initial Commit

---
+++ BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by Hector Reluz on 7/15/23.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
